5cb155e6d21693be15d5de31087d4a5be909b2f1
vanson0503 <vanson05032002@gmail.com>
2024-06-04 11:39:13 +0700

    push

--- server.js
+++ server.js
@@ -13,16 +13,27 @@ app.get('/view', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'view.html'));
 });
 
+let livestreamAvailable = false;
+let streamUrl;
+
 io.on('connection', socket => {
     console.log('a user connected', socket.id);
 
     socket.on('join', room => {
         socket.join(room);
-        socket.to(room).emit('user-joined', socket.id);
+        socket.to(room).emit('user-joined');
     });
 
     socket.on('signal', ({ room, data }) => {
         socket.to(room).emit('signal', data);
+    });
+
+    socket.on('request-stream', room => {
+        if (livestreamAvailable) {
+            socket.emit('stream-available', streamUrl);
+        } else {
+            socket.emit('no-stream-available');
+        }
     });
 
     socket.on('disconnect', () => {
@@ -32,3 +43,9 @@ io.on('connection', socket => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// Mock livestream availability
+setTimeout(() => {
+    livestreamAvailable = true;
+    streamUrl = 'http://example.com/livestream';
+}, 5000);
